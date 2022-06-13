e9bb5095a7eb94903455a50ce421b19994ec81f5
Dibyanshu Jaiswal <uname2334@gmail.com>
2022-06-13 18:17:40 +0530

    test

--- index.js
+++ index.js
@@ -16,15 +16,25 @@ const positions = new Map();
 
 io.on('connection', (socket) => {
     console.log(`${socket.id} connected`);
-    console.log(positions);
+    // console.log(positions);
 
     socket.on('location', (payload) => {
         positions.set(socket.id, { ...payload });
+        const locations = [];
+        positions.forEach((value, key) => {
+            locations.push({ id: key, ...value });
+        });
+        io.emit('takeLocations', { locations });
     });
 
     socket.on('disconnect', () => {
         console.log(`${socket.id} disconnected`);
         positions.delete(socket.id);
+        const locations = [];
+        positions.forEach((value, key) => {
+            locations.push({ id: key, ...value });
+        });
+        io.emit('takeLocations', { locations });
     });
 });
 
